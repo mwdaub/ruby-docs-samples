d1067e2f356a21a09d2c32f13b3b1577b29b4aa6
mwdaub <mwdaub@gmail.com>
2018-09-06 13:40:41 -0700

    test commit

--- dlp/sample.rb
+++ dlp/sample.rb
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Test edit.
 
 def inspect_string project_id: nil, content: nil, max_findings: 0
   # [START dlp_inspect_string]
